138839e8cd83653fc8bca31b7a0f7643bb3ec605
Katherine Richards <katherinerichards@cs-wifiopen-88.cs.wisc.edu>
2015-10-23 14:34:47 -0500

    Initial Commit

---
+++ HW2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HW2
+//
+//  Created by Katherine Richards on 10/23/15.
+//  Copyright © 2015 Katherine Richards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
